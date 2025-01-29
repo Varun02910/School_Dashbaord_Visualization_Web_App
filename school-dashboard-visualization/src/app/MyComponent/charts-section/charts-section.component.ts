bc79ad18b6724ac65ca08ac8c27a4f54574919ca
R Hari Narayan <rhari.narayan@iic.ac.in>
2025-01-29 16:14:44 +0530

    MapSection and ChartsSection components created

---
+++ school-dashboard-visualization/src/app/MyComponent/charts-section/charts-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-charts-section',
+  imports: [],
+  templateUrl: './charts-section.component.html',
+  styleUrl: './charts-section.component.scss'
+})
+export class ChartsSectionComponent {
+
+}
--- school-dashboard-visualization/src/app/app.component.ts
+++ school-dashboard-visualization/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DashboardHeadingComponent } from './MyComponent/dashboard-heading/dashboard-heading.component';
+import { MapSectionComponent } from "./MyComponent/map-section/map-section.component";
+import { ChartsSectionComponent } from "./MyComponent/charts-section/charts-section.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DashboardHeadingComponent],
+  imports: [RouterOutlet, DashboardHeadingComponent, MapSectionComponent, ChartsSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
